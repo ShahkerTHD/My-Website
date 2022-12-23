cf1df1a3c0100b310cc62bb3531d8cf53acb796c
Coder Shaheer <118484201+TheHackerDude1083@users.noreply.github.com>
2022-12-23 03:03:24 +0000

    Added neon effect to Home button.Changed Contacts page's picture

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # My-Website
 
 This is the New TheHackerdude1083's website!
+The original was forked from Bookie0.
+If you want to chat my site has its very own live chat.
+This site now is connected to the domain at "TheHackerDude1083.is-a.dev"
--- script.js
+++ script.js
@@ -1,82 +1,82 @@
 function sunToMoon() {
-	document.querySelector(".theme-icon").setAttribute("data-feather", "moon");
-	feather.replace()
+  document.querySelector(".theme-icon").setAttribute("data-feather", "moon");
+  feather.replace()
 }
 
 function moonToSun() {
-	document.querySelector(".theme-icon").setAttribute("data-feather", "sun");
-	feather.replace()
+  document.querySelector(".theme-icon").setAttribute("data-feather", "sun");
+  feather.replace()
 }
 
 var currentTheme;
 
 function checkTheme() {
-	currentTheme = localStorage.getItem("theme");
+  currentTheme = localStorage.getItem("theme");
 
-	console.log(currentTheme);
-			
-	if (currentTheme === null) {
-		console.log("Null, set to light.");
-		localStorage.setItem("theme", "light-theme");
-		document.body.classList.add("light-theme");
-		sunToMoon();
+  console.log(currentTheme);
 
-		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 		{
-	    console.log("null dark, set to dark");
-			document.body.classList.add("dark-theme");
-			moonToSun();
-			localStorage.setItem("theme", "dark-theme");
-		}
+  if (currentTheme === null) {
+    console.log("Null, set to light.");
+    localStorage.setItem("theme", "light-theme");
+    document.body.classList.add("light-theme");
+    sunToMoon();
 
-		else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) 		{
-	    console.log("null light, set to light");
-			document.body.classList.add("light-theme");
-			sunToMoon();
-			localStorage.setItem("theme", "light-theme");
-		}
-	}
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      console.log("null dark, set to dark");
+      document.body.classList.add("dark-theme");
+      moonToSun();
+      localStorage.setItem("theme", "dark-theme");
+    }
 
-	else if (currentTheme === "light-theme") {
-		console.log("light, set to light");
-		document.body.classList.add("light-theme");
-		sunToMoon();
-	}
+    else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+      console.log("null light, set to light");
+      document.body.classList.add("light-theme");
+      sunToMoon();
+      localStorage.setItem("theme", "light-theme");
+    }
+  }
 
-	else if (currentTheme === "dark-theme") {
-		console.log("dark, set to dark");
-		document.body.classList.add("dark-theme");
-		moonToSun();
-	}
+  else if (currentTheme === "light-theme") {
+    console.log("light, set to light");
+    document.body.classList.add("light-theme");
+    sunToMoon();
+  }
 
-	else {
-		localStorage.clear();
-	}
+  else if (currentTheme === "dark-theme") {
+    console.log("dark, set to dark");
+    document.body.classList.add("dark-theme");
+    moonToSun();
+  }
+
+  else {
+    localStorage.clear();
+  }
 }
 
 function changeTheme() {
-	currentTheme = localStorage.getItem("theme");
+  currentTheme = localStorage.getItem("theme");
 
-	console.log("theme before change:")
-	console.log(currentTheme);
+  console.log("theme before change:")
+  console.log(currentTheme);
 
-	if (currentTheme === "light-theme") {
-		document.body.classList.remove("light-theme");
-		document.body.classList.add("dark-theme");
-		localStorage.setItem("theme", "dark-theme");	
-		moonToSun();
-	}
+  if (currentTheme === "light-theme") {
+    document.body.classList.remove("light-theme");
+    document.body.classList.add("dark-theme");
+    localStorage.setItem("theme", "dark-theme");
+    moonToSun();
+  }
 
-	else if (currentTheme === "dark-theme") {
-		document.body.classList.remove("dark-theme");
-		document.body.classList.add("light-theme");
-		localStorage.setItem("theme", "light-theme");	
-		sunToMoon();
-	}
+  else if (currentTheme === "dark-theme") {
+    document.body.classList.remove("dark-theme");
+    document.body.classList.add("light-theme");
+    localStorage.setItem("theme", "light-theme");
+    sunToMoon();
+  }
 
-	currentTheme = localStorage.getItem("theme");
-	console.log("current theme after change:")
-	console.log(currentTheme);
-	
+  currentTheme = localStorage.getItem("theme");
+  console.log("current theme after change:")
+  console.log(currentTheme);
+
 }
 
 checkTheme();
